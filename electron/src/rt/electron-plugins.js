9f90b01cab9311a474bb7e19f635d3dc5ad20e94
Dirk Gausmann <di.gaus@gmx.de>
2021-09-01 00:56:39 +0200

    Fixes

--- electron/src/rt/electron-plugins.js
+++ electron/src/rt/electron-plugins.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const CapacitorCommunityFileServer = require('C:\\Users\\User\\Documents\\Git\\capacitor-plugin-demo\\electron\\node_modules\\@capacitor-community\\file-server\\electron\\dist\\plugin.js')
-const CapacitorCommunityWifi = require('C:\\Users\\User\\Documents\\Git\\capacitor-plugin-demo\\electron\\node_modules\\@capacitor-community\\wifi\\electron\\dist\\plugin.js')
+const CapacitorCommunityFileServer = require('/Users/dirkgausmann/Documents/git/capacitor-plugin-demo/electron/node_modules/@capacitor-community/file-server/electron/dist/plugin.js')
+const CapacitorCommunityWifi = require('/Users/dirkgausmann/Documents/git/capacitor-plugin-demo/electron/node_modules/@capacitor-community/wifi/electron/dist/plugin.js')
 
 module.exports = {
   CapacitorCommunityFileServer,
